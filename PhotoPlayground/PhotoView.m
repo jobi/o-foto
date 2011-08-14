8f7f0fb79361a487e5315cee3b3cbb2d6b9a4e0f
Johan Bilien <jobi@via.ecp.fr>
2011-08-13 23:41:45 -0400

    Add moving of the pictures

--- PhotoPlayground/PhotoView.m
+++ PhotoPlayground/PhotoView.m
@@ -10,6 +10,9 @@
 
 @interface PhotoView()
 
+- (void)handleTranslationTouches:(NSSet *)touches;
+- (void)handleSimilitudeTouches:(NSSet *)touches;
+
 @end
 
 @implementation PhotoView
@@ -36,6 +39,9 @@
         
         CGFloat rotationAngle = 60.0f * (M_PI/180) * rand()/RAND_MAX - (30.0f * (M_PI/180));
         [layer setAffineTransform:CGAffineTransformMakeRotation(rotationAngle)];
+        
+        self.userInteractionEnabled = YES;
+        self.multipleTouchEnabled = YES;
     }
     
     return self;
@@ -66,6 +72,94 @@
     [self.delegate photoView:self imageLoaded:image];
 
     [image release];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.superview bringSubviewToFront:self];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSSet *touchesInView = [event touchesForView:self];
+    
+    switch ([touchesInView count]) {
+        case 1:
+            [self handleTranslationTouches:touchesInView];
+            break;
+        case 2:
+            [self handleSimilitudeTouches:touchesInView];
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)handleTranslationTouches:(NSSet *)touches
+{
+    CGPoint point;
+    CGPoint previous;
+    
+    for (UITouch *touch in touches) {
+        point = [touch locationInView:self.superview];
+        previous = [touch previousLocationInView:self.superview];
+    }
+    
+    CGPoint position = self.layer.position;
+    
+    position.x += point.x - previous.x;
+    position.y += point.y - previous.y;
+    
+    self.layer.position = position;
+}
+
+- (void)handleSimilitudeTouches:(NSSet *)touches
+{
+    CGAffineTransform m;
+    
+    CGFloat x1, y1, x2, y2;
+    CGFloat x1p, y1p, x2p, y2p;
+    
+    UITouch *touch = [[touches allObjects]objectAtIndex:0];
+    
+    CGPoint point = [touch locationInView:self.superview];
+    CGPoint previous = [touch previousLocationInView:self.superview];
+    
+    x1 = previous.x - self.center.x;
+    y1 = previous.y - self.center.y;
+    
+    x1p = point.x - self.center.x;
+    y1p = point.y - self.center.y;
+    
+    touch = [[touches allObjects]objectAtIndex:1];
+    
+    point = [touch locationInView:self.superview];
+    previous = [touch previousLocationInView:self.superview];
+    
+    x2 = previous.x - self.center.x;
+    y2 = previous.y - self.center.y;
+    
+    x2p = point.x - self.center.x;
+    y2p = point.y - self.center.y;
+    
+    CGFloat d = (y1-y2)*(y1-y2) + (x1-x2)*(x1-x2);
+	if (d < 0.1) {
+        m = CGAffineTransformMakeTranslation(x1p-x1, y1p-y1);
+    }
+    
+	CGFloat a = (y1-y2)*(y1p-y2p) + (x1-x2)*(x1p-x2p);
+	CGFloat b = (y1-y2)*(x1p-x2p) - (x1-x2)*(y1p-y2p);
+	CGFloat tx = (y1*x2 - x1*y2)*(y2p-y1p) - (x1*x2 + y1*y2)*(x1p+x2p) + x1p*(y2*y2 + x2*x2) + x2p*(y1*y1 + x1*x1);
+	CGFloat ty = (x1*x2 + y1*y2)*(-y2p-y1p) + (y1*x2 - x1*y2)*(x1p-x2p) + y1p*(y2*y2 + x2*x2) + y2p*(y1*y1 + x1*x1);
+	
+    m = CGAffineTransformMake(a/d, -b/d, b/d, a/d, tx/d, ty/d);
+    
+    [self.layer setAffineTransform:CGAffineTransformConcat([self.layer affineTransform],  m)];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
 }
 
 - (void)dealloc
